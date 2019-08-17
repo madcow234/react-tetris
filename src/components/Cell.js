7ef60bca9b6ed1992435b278e2cbef969d592db6
MadCow234 <MadCow234@gmail.com>
2019-08-17 16:00:46 -0500

    Modify <Cell/> to use new <StyledCell/> component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+// Components
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../services/tetrominos';
+
 const Cell = ({type}) => (
-  <div>Cell</div>
+  <StyledCell type={type} color={TETROMINOS[type].color}>cell</StyledCell>
 )
 
 export default Cell;
