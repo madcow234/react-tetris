3ebc5108bd49c13df9e112a1770cd753cf69e6c5
MadCow234 <MadCow234@gmail.com>
2019-08-17 17:23:20 -0500

    Modify <StartButton/> to use new <StyledStartButton/> component

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+// Styled Components
+import { StyledStartButton } from './styles/StyledStartButton';
+
 const StartButton = ({callback}) => (
-  <div>Start Game</div>
+  <StyledStartButton onClick={callback}>Start Game</StyledStartButton>
 )
 
 export default StartButton;
