1baa1fbaa8a2dd3c6e92ba8c4c34eabd39a8f458
MadCow234 <MadCow234@gmail.com>
2019-08-17 17:06:33 -0500

    Modify <Stage/> to use new <StyledStage/> component

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -3,10 +3,13 @@ import React from 'react';
 // Components
 import Cell from './Cell';
 
+// Styled Components
+import { StyledStage } from './styles/StyledStage';
+
 const Stage = ({stage}) => (
-  <div>
+  <StyledStage width={stage[0].length} height={stage.length}>
     {stage.map(row => row.map((cell, index) => <Cell key={index} type={cell[0]} />))}
-  </div>
+  </StyledStage>
 )
 
 export default Stage;
