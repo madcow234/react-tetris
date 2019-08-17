da861ce768cb221b7ed5a0c445c623334de7472a
MadCow234 <MadCow234@gmail.com>
2019-08-17 16:42:17 -0500

    Wrap the game screen with new styled components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,19 +6,25 @@ import Display from './Display';
 import StartButton from './StartButton';
 import {createStage} from '../services/gameHelpers';
 
+// Styled Components
+import { StyledTetrisWrapper } from './styles/StyledTetrisWrapper';
+import { StyledTetris } from './styles/StyledTetris';
+
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   )
 };
 
---
+++ src/components/styles/StyledTetrisWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import bgImg from '../../img/bg.png'
+
+export const StyledTetrisWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: url(${bgImg}) #000;
+  background-size: cover;
+  overflow: hidden;
+`
