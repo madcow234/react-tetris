c9b804c74b052c77d9efc275a267e6ec63af5b34
MadCow234 <MadCow234@gmail.com>
2019-08-18 16:57:27 -0500

    Add custom hook for saving score, rows, and level states

---
+++ src/hooks/useGameStatus.js
@@ -0,0 +1,25 @@
+import { useCallback, useState, useEffect } from 'react';
+
+export const useGameStatus = rowsCleared => {
+  const [score, setScore] = useState(0);
+  const [rows, setRows] = useState(0);
+  const [level, setLevel] = useState(0);
+
+  const linePoints = [40, 100, 300, 1200];
+
+  const calcScore = useCallback(() => {
+    // We have a score to calculate
+    if (rowsCleared > 0) {
+      // This is how the original Tetris score is calculated
+      setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1));
+      // Keep track of total rows cleared during the game
+      setRows(prev => prev + rowsCleared);
+    }
+  }, [level, linePoints, rowsCleared]);
+
+  useEffect(() => {
+    calcScore();
+  }, [calcScore, rowsCleared, score]);
+
+  return [score, setScore, rows, setRows, level, setLevel]
+}
