2211e0b338cf2c1b2f27b13ecb263ec344821c9d
MadCow234 <MadCow234@gmail.com>
2019-08-18 16:47:11 -0500

    Add functionality to clear rows that are filled with tetrominos

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,30 @@ import { createStage } from '../services/gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = newStage =>
+      newStage.reduce((acc, row) => {
+        // A row should be cleared when it is filled with tetrominos
+        // A row will be filled with tetrominos when the type of each cell is not 0
+        // If findIndex does not find any cells with type 0, it will return -1
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          // Increment the total number of rows cleared in this round
+          setRowsCleared(prev => prev + 1);
+          // Add a new clean row to the top of the stage
+          acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          // Return the accumulator without adding the current row
+          return acc;
+        }
+
+        // Otherwise, add the current row and return the accumulator
+        acc.push(row);
+        return acc;
+      }, []);
+
     const updateStage = prevStage => {
       // First, flush the stage
       const newStage = prevStage.map(row =>
@@ -26,13 +48,15 @@ export const useStage = (player, resetPlayer) => {
       // Then check if we collided
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
 
       return newStage;
     }
 
     setStage(prev => updateStage(prev))
+
   }, [player, resetPlayer])
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 }
