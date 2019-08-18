de30297b27d65ef5066f6937b4e41539bb1d0c11
MadCow234 <MadCow234@gmail.com>
2019-08-18 14:45:07 -0500

    Add checkCollision method

--- src/services/gameHelpers.js
+++ src/services/gameHelpers.js
@@ -7,3 +7,24 @@ export const STAGE_HEIGHT = 20; // numRows
  *  cell[1]: instruction for rendering, one of ['clear', 'merge'] - informs whether a cell has collided with another cell 
  */
 export const createStage = () => Array(STAGE_HEIGHT).fill(Array(STAGE_WIDTH).fill([0, 'clear']))
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY}) => {
+  for (let y = 0; y < player.tetromino.length; y++) {
+    for (let x = 0; x < player.tetromino[y].length; x++) {
+      // Check that we're on an actual tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+          // Check that our move is inside the game area's height (y)
+          // We shouldn't go through the bottom of the play area
+          !stage[y + player.pos.y + moveY] || 
+          // Check that our move is inside the game area's width (x)
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          // Check that the cell we're moving to is not set to 'clear'
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+}
