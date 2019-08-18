24a9eaa3c64f5d29855503a6fd8aa452114408e8
MadCow234 <MadCow234@gmail.com>
2019-08-18 17:11:57 -0500

    Performance optimization using React.memo()

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -5,7 +5,8 @@ import { TETROMINOS } from '../services/tetrominos';
 import { StyledCell } from './styles/StyledCell';
 
 const Cell = ({type}) => (
-  <StyledCell type={type} color={TETROMINOS[type].color} />
+  <StyledCell type={type} color={TETROMINOS[type].color} >{console.log("re-render")}</StyledCell>
 )
 
-export default Cell;
+// Export the cell with React.memo() so the cell only re-renders when it changes
+export default React.memo(Cell);
