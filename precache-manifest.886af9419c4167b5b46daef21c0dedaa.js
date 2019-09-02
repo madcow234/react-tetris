45f6012805abbcb0e15c9e9afb84d0d00b82a91b
MadCow234 <MadCow234@gmail.com>
2019-09-02 18:10:44 -0500

    Updates

---
+++ precache-manifest.886af9419c4167b5b46daef21c0dedaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf07bee58127c87aa12c9629c0e0a696",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "feaccf02c0fa00fdec71",
+    "url": "/react-tetris/static/css/main.be863ed6.chunk.css"
+  },
+  {
+    "revision": "7d0855a17b2ed294e15b",
+    "url": "/react-tetris/static/js/2.267a5cdf.chunk.js"
+  },
+  {
+    "revision": "feaccf02c0fa00fdec71",
+    "url": "/react-tetris/static/js/main.21bd581b.chunk.js"
+  },
+  {
+    "revision": "3cbf365fbc4eeca393c0",
+    "url": "/react-tetris/static/js/runtime~main.1a1357f3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
